f4317d29896a1f200c0b5fab92b0f59bfb2eac4e
Maksim Vlasiuk <maksym.lang@gmail.com>
2025-04-26 21:13:56 +0200

    first commit

---
+++ src/js/components/Footer.js
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+
+import './../../styles/Fotter.scss'
+
+class Fotter extends Component {
+
+    render() {
+        return (
+            <div className="Fotter">
+                <p>Fotter</p>
+            </div>
+        )
+    }
+}
+
+export default Fotter
