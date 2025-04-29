e6b6a6b7695d53ec2d7240e7d75861b85f3ac7c2
Maksim Vlasiuk <maksym.lang@gmail.com>
2025-04-30 00:37:50 +0200

    Fix Nav,Header add section about-us and i18n for text

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -1,17 +1,20 @@
 import { ReactComponent as LogoIcon} from '../../assets/images/icons/coffee-beans-logo.svg';
+import { useTranslation} from "react-i18next";
 
 import '../../styles/components/Nav.scss'
 
 function Nav() {
+    const { t } = useTranslation();
+
     return (
         <nav className="nav">
             <div className="nav__menu">
                 <div className="nav__logo text-white text-xs font-normal">
                     <LogoIcon className="nav__icon"/>
-                    Coffee house
+                    {t('nav.logo')}
                 </div>
-                <div className='nav__item text-white text-xs font-normal'>Our coffee</div>
-                <div className='nav__item text-white text-xs font-normal'>For your pleasure</div>
+                <div className='nav__item text-white text-xs font-normal'>{t('nav.ourCoffee')}</div>
+                <div className='nav__item text-white text-xs font-normal'>{t('nav.pleasure')}</div>
             </div>
         </nav>
     )
--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,5 +1,7 @@
 import {Component} from 'react';
-import { ReactComponent as LogoIcon} from '../../assets/images/icons/coffee-beans white.svg';
+import { ReactComponent as WhiteCoffeeBean} from '../../assets/images/icons/coffee-beans white.svg';
+import { ReactComponent as BlackCoffeeBean} from '../../assets/images/icons/coffee-beans black.svg';
+import { withTranslation } from 'react-i18next';
 
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
@@ -7,22 +9,32 @@ import '../../styles/Home/Home.scss'
 
 class Home extends Component {
     render() {
+        const { t } = this.props;
         return (
             <>
                 <header className="home">
                     <Nav/>
                     <div className='home__content'>
-                        <h1 className="home__title">Everything You Love About Coffee</h1>
+                        <h1 className="home__title text-white font-bold text-lg text-shadow">{t('home.title')}</h1>
                         <div className="home__transition">
-                            <div className="home__transition__line text-lg"></div>
-                            <LogoIcon className="home__transition__icon"/>
-                            <div className="home__transition__line"></div>
+                            <div className="home__line"></div>
+                            <WhiteCoffeeBean className="home__icon" />
+                            <div className="home__line"></div>
                         </div>
-                        <h2 className="home__subtitle text-md text-white">We makes every day full of energy and taste<br/>Want to try our beans?</h2>
+                        <h2 className="home__subtitle text-md text-white text-preline">{t('home.subtitle')}</h2>
+                        <button className="home__btn text-sm font-bold text-white">{t('button.more')}</button>
                     </div>
                 </header>
-                <section className="home__section">
-
+                <section className="about-us">
+                    <div className="about-us__content">
+                        <h2 className="about-us__title text-black text-md font-normal">{t('about.title')}</h2>
+                        <div className="about-us__transition">
+                            <div className="about-us__line"></div>
+                            <BlackCoffeeBean className="about-us__icon" />
+                            <div className="about-us__line"></div>
+                        </div>
+                        <p className="about-us__text text-sm text-black text-preline">{t('about.text')}</p>
+                    </div>
                 </section>
                 <section className="home__section">
 
@@ -33,4 +45,4 @@ class Home extends Component {
     }
 }
 
-export default Home;
+export default withTranslation() (Home);
